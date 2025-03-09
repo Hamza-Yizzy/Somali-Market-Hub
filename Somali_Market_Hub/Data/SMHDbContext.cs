e48886501ce2edfdb3e374b4af1557f52c5cd19a
Hamza-Yizzy <izzoG119@gmail.com>
2025-03-09 09:26:08 -0700

    Somali Market Hub : a digital market platform that built in ASP.NET Core MVC

---
+++ Somali_Market_Hub/Data/SMHDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Somali_Market_Hub.Data
+{
+    public class SMHDbContext : DbContext
+    {
+        public SMHDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected SMHDbContext()
+        {
+        }
+    }
+}
---
+++ Somali_Market_Hub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Somali_Market_Hub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
